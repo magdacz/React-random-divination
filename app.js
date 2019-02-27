3b961659567955614eb332e037cbb87df3deec20
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-02-27 14:29:08 +0100

    Add handleChangeInput & handleAddOption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ class App extends React.Component {
     state = {
         options: ["Lepiej dobrze przemyśl swoje zamiary!", "Czasami żeby zrobić krok do przodu trzeba najpierw zrobić dwa kroki w tył!", "Pamiętaj, czasami najprostsze rozwiązania są najlepsze!", "Bądź dobrej myśli, nigdy nie wiesz co przyniesie nowy dzień!", "Odpocznij, w życiu potrzebna jest równowaga!"],
         option: "",
+        value: ""
     }
     
    handleShowOption = () => {
@@ -11,15 +12,38 @@ class App extends React.Component {
        this.setState({
            option: this.state.options[index]
        })
-       
    }
-
+   
+   handleChangeInput = (e) => {
+       this.setState({
+           value: e.target.value
+       })
+   }
+   
+   
+   handleAddOption = () => {
+       if(this.state.value === "") return alert("Musisz wpisać tekst!")
+       const options = [...this.state.options]
+       options.push(this.state.value)
+       
+       this.setState({
+           options,
+           value: "",
+       })
+       
+       alert(`Wróżba została dodana. Aktualnie dostępne wróżby: 
+    ${options.join(" ,")}`)
+   }
+   
     render() {
         
         return (
             <React.Fragment>
                 <button onClick={this.handleShowOption}>Losuj wróżbę</button>
-                { this.state.option && <h1>{this.state.option}</h1> }
+                {this.state.option && <h1>{this.state.option}</h1>} <br/>
+                <input type="text" value={this.state.value} onChange={this.handleChangeInput}></input>
+                <button onClick={this.handleAddOption}>Dodaj wróżbę</button>
+            
             </React.Fragment>
         )
     }
